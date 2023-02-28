38b801091014aa83e0aff463fa4c02527b654fe7
João Leonardo <joaoleonardo9921@gmail.com>
2023-02-28 19:44:47 -0300

    Criando e aplicando métodos de validação

--- Jogo-De-Xadrez-Java/Sistema-Xadrez/src/tabuleiro/Tabuleiro.java
+++ Jogo-De-Xadrez-Java/Sistema-Xadrez/src/tabuleiro/Tabuleiro.java
@@ -5,6 +5,8 @@ public class Tabuleiro {
 	private Integer colunas;
 	private Peca[][] pecas;
 	public Tabuleiro(Integer linha, Integer coluna) {
+		if(linha < 1 || coluna < 1)
+			throw new TabuleiroException("Erro ao criar tabuleiro é necessario que haja pelo menos 1 linha e 1 coluna.");
 		this.linhas = linha;
 		this.colunas = coluna;
 		pecas = new Peca[linhas][colunas];
@@ -12,26 +14,37 @@ public class Tabuleiro {
 	public Integer getLinhas() {
 		return linhas;
 	}
-	public void setLinhas(Integer linhas) {
-		this.linhas = linhas;
-	}
 	public Integer getColunas() {
 		return colunas;
 	}
-	public void setColunas(Integer colunas) {
-		this.colunas = colunas;
-	}
-	
 	public Peca peca(Integer linha, Integer coluna) {
+		if(!PosicaoExiste(linha, coluna))
+			throw new TabuleiroException("Não existe essa posição no tabuleiro");
 		return pecas[linha][coluna];
 	}
 	public Peca peca(Posicao posicao) {
+		if(!PosicaoExiste(posicao))
+			throw new TabuleiroException("Não existe essa posição no tabuleiro");
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	public void colocarPeca(Peca peca, Posicao posicao) {
+		if(PecaExiste(posicao))
+			throw new TabuleiroException("Já existe uma peça na posição " + posicao);
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
-	
+
+	private Boolean PosicaoExiste(int linha, int coluna){
+		return linha >= 0 && linha < linhas && coluna >=0  && coluna < colunas;
+	}
+	public Boolean PosicaoExiste(Posicao posicao){
+		return PosicaoExiste(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public Boolean PecaExiste(Posicao posicao){
+		if(!PosicaoExiste(posicao))
+			throw new TabuleiroException("Não existe essa posição no tabuleiro");
+		return peca(posicao) != null;
+	}
 }
