a0fe74fc0762d8aa32dd900c87d40d363ad17853
João Leonardo <joaoleonardo9921@gmail.com>
2023-03-01 19:53:24 -0300

    Adicionando método de limpar o terminal [inacabado]

--- Jogo-De-Xadrez-Java/Sistema-Xadrez/src/app/UI.java
+++ Jogo-De-Xadrez-Java/Sistema-Xadrez/src/app/UI.java
@@ -28,6 +28,10 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void limparTela(){
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 	public static XadrezPosicao lerXadrezPosicao(Scanner input){
 		try {
 			String s = input.nextLine();
