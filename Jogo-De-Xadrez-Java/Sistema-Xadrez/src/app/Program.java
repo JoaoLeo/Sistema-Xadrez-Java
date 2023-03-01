808bf21de8388635f9e6116264015b22ee924e80
João Leonardo <joaoleonardo9921@gmail.com>
2023-03-01 19:43:40 -0300

    Testando movimentação

--- Jogo-De-Xadrez-Java/Sistema-Xadrez/src/app/Program.java
+++ Jogo-De-Xadrez-Java/Sistema-Xadrez/src/app/Program.java
@@ -2,12 +2,26 @@ package app;
 
 import tabuleiro.Tabuleiro;
 import xadrez.PartidaDeXadrez;
+import xadrez.PecaDeXadrez;
+import xadrez.XadrezPosicao;
+
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
-		UI.printTabuleiro(partidaDeXadrez.getPecas());
-	}
+		while (true){
+			UI.printTabuleiro(partidaDeXadrez.getPecas());
+			System.out.println();
+			System.out.print("Origem: ");
+			XadrezPosicao origem = UI.lerXadrezPosicao(input);
 
+			System.out.println();
+			System.out.print("Destino: ");
+			XadrezPosicao destino = UI.lerXadrezPosicao(input);
+			PecaDeXadrez pecaCapturada = partidaDeXadrez.performaMovimentoDeXadrez(origem,destino);
+		}
+	}
 }
--- Jogo-De-Xadrez-Java/Sistema-Xadrez/src/app/UI.java
+++ Jogo-De-Xadrez-Java/Sistema-Xadrez/src/app/UI.java
@@ -2,6 +2,10 @@ package app;
 
 import xadrez.Cores;
 import xadrez.PecaDeXadrez;
+import xadrez.XadrezPosicao;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -23,6 +27,18 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+	public static XadrezPosicao lerXadrezPosicao(Scanner input){
+		try {
+			String s = input.nextLine();
+			char coluna = s.charAt(0);
+			int linha = Integer.parseInt(s.substring(1)); //Recorta a string a partir do index 1 e converte para int
+			return new XadrezPosicao(coluna, linha);
+		} catch (RuntimeException e){
+			throw new InputMismatchException("Erro ao ler posição, valore válidos a1 - h8");
+		}
+	}
+
 	public static void printTabuleiro(PecaDeXadrez[][] peca){
 		for(int i = 0; i < peca.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -46,5 +62,5 @@ public class UI {
 			}
 		}
 		System.out.print(" ");
-	} 
+	}
 }
