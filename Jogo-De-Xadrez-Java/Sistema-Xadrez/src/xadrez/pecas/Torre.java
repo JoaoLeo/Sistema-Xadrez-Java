ce7bb8511b623174eb59e8fdd06ebaea456e9dc3
João Leonardo <joao.ferreira@hepta.com.br>
2023-03-02 09:14:26 -0300

    Adicionando lógica de movimentação da Torre

--- Jogo-De-Xadrez-Java/Sistema-Xadrez/src/xadrez/pecas/Torre.java
+++ Jogo-De-Xadrez-Java/Sistema-Xadrez/src/xadrez/pecas/Torre.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Cores;
 import xadrez.PecaDeXadrez;
@@ -20,6 +21,43 @@ public class Torre  extends PecaDeXadrez {
 		boolean[][] mat =
 				new boolean[getTabuleiro().getLinhas()]
 						[getTabuleiro().getColunas()];
+		Posicao p = new Posicao(0,0);
+
+		// Vertical - para cima
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)){
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p))
+			mat[p.getLinha()][p.getColuna()] = true;
+
+		// Vertical - para baixo
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)){
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p))
+			mat[p.getLinha()][p.getColuna()] = true;
+
+		// Horizontal - para esquerda
+		p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)){
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p))
+			mat[p.getLinha()][p.getColuna()] = true;
+
+		// Horizontal - para direita
+		p.setValores(posicao.getLinha(), posicao.getColuna()  + 1);
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)){
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() + 1);
+		}
+		if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p))
+			mat[p.getLinha()][p.getColuna()] = true;
 		return mat;
 	}
 }
